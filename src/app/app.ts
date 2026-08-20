b9e1f6c710a2d2f7887850a19bfa859bc383a53b
IsisMarieli <Isismarieli1@gmail.com>
2026-08-20 14:43:41 -0300

    add home

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { Home } from './components/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ Header, Home , Footer ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
