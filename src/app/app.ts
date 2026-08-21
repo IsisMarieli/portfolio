c469db99f5e6812a183c9fa0877820c22e710e31
IsisMarieli <Isismarieli1@gmail.com>
2026-08-21 15:08:06 -0300

    add about

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
 import { Home } from './components/home/home';
+import { About } from './components/about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [ Header, Home , Footer ],
+  imports: [ Header, Home , Footer, About ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
