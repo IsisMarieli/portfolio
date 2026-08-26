cefad77ff8f662130febfd793c9125192f7c51e5
IsisMarieli <Isismarieli1@gmail.com>
2026-08-26 15:30:15 -0300

    add project

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
 import { Home } from './components/home/home';
 import { About } from './components/about/about';
+import { Project } from './components/project/project';
 
 @Component({
   selector: 'app-root',
-  imports: [ Header, Home , Footer, About ],
+  imports: [ Header, Home , Footer, About, Project ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
