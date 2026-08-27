29d43a40fb8dcfe7921ddb0f48a8627d038e5396
IsisMarieli <Isismarieli1@gmail.com>
2026-08-27 15:08:16 -0300

    add experience

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Footer } from './components/footer/footer';
 import { Home } from './components/home/home';
 import { About } from './components/about/about';
 import { Project } from './components/project/project';
+import { Experience } from './components/experience/experience'; 
 
 @Component({
   selector: 'app-root',
-  imports: [ Header, Home , Footer, About, Project ],
+  imports: [ Header, Home , Footer, About, Project, Experience ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
